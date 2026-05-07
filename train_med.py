1862849b18062867be400cdd0fca6c4ee12b44b0
MartinHamas1 <martinhamas7@gmail.com>
2026-05-07 14:04:46 +0200

    train_med.py

--- train_med.py
+++ train_med.py
@@ -1,28 +1,38 @@
-    out_dir = 'out-medical'
-    eval_interval = 500
-    log_interval = 10
+out_dir = 'out-medical-scratch'
+eval_interval = 250
+log_interval = 10
+eval_iters = 100
+always_save_checkpoint = True
 
-    dataset = 'med'   
-    batch_size = 32
-    block_size = 128
+dataset = 'med'
+gradient_accumulation_steps = 1
+batch_size = 32
+block_size = 256
 
-    n_layer = 6
-    n_head = 6
-    n_embd = 384
-    dropout = 0.1
+n_layer = 6
+n_head = 6
+n_embd = 384
+dropout = 0.1
+bias = False
 
-    learning_rate = 3e-4
-    max_iters = 5000
-    lr_decay_iters = 5000
-    min_lr = 3e-5
-    warmup_iters = 100
+learning_rate = 6e-4
+max_iters = 5000
+weight_decay = 0.1
+beta1 = 0.9
+beta2 = 0.95
+grad_clip = 1.0
 
-    weight_decay = 0.1
-    beta1 = 0.9
-    beta2 = 0.95
+decay_lr = True
+warmup_iters = 200
+lr_decay_iters = 5000
+min_lr = 6e-5
 
-    device = 'cuda'  
+init_from = 'scratch'
 
-    wandb_log = True
-    wandb_project = 'nanogpt-med'
-    wandb_run_name = 'med-run-1'
+device = 'cuda'
+dtype = 'bfloat16'
+compile = False
+
+wandb_log = True
+wandb_project = 'nanogpt-med'
+wandb_run_name = 'med-scratch-run'
